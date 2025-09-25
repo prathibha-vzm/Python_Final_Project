3402d169c556d615e3afcdf501fa1178a2d94c5e
prathibha-vzm <vigneswaranprathibha@gmail.com>
2025-09-25 17:23:03 +0530

    Delete data/convert_ods_csv.py

--- data/convert_ods_csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def convert_ods_to_csv(ods_file, csv_file, sheet=0):
-    df = pd.read_excel(ods_file, engine="odf", sheet_name=sheet)
-    df.to_csv(csv_file, index=False)
-
-if __name__=="__main__":
-    convert_ods_to_csv("test_contact_information.ods", "test_contact_information.csv")
